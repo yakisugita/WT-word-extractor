118dd9c2a5a245a3355cedd628c43d3565908dcd
yakisugita <yakisugita@gmail.com>
2024-12-07 16:03:15 +0900

    パートが複数あっても正常に抽出できるように修正

--- extractor.js
+++ extractor.js
@@ -1,3 +1,6 @@
+// ここに入れたパート内のワードは全て抽出後に入る
+const skip_parts = []
+
 const fs = require("fs")
 const { XMLParser, XMLBuilder } = require("fast-xml-parser")
 
@@ -24,23 +27,38 @@ const xml = fs.readFileSync(filePath)
 const xmlObj = perser.parse(xml)
 const outWordArray = []
 
-xmlObj.Words.Part.forEach(part => {
-    part.Word.forEach(word => {
-        // 抽出対象の文字を含んでいるか判定
-        let count = 0
-        extractWords.forEach(extractWord => {
-            if (word.Characters.includes(extractWord)) {
-                count++
+// Part1 前半, Part2 後半
+console.log("[original]")
+xmlObj.Words.Part.forEach((part, i) => {
+    console.log(`Part ${i} : ${part.Word.length}`)
+    outWordArray[i] = []
+    if (skip_parts.includes(i)) {
+        // パススルーする
+        outWordArray[i] = part.Word
+    } else {
+        part.Word.forEach(word => {
+            // 抽出対象の文字を含んでいるか判定
+            let count = 0
+            extractWords.forEach(extractWord => {
+                if (word.Characters.includes(extractWord)) {
+                    count++
+                }
+            })
+            if (count > 0) {
+                // console.log(word.Characters)
+                outWordArray[i].push(word)
             }
         })
-        if (count > 0) {
-            console.log(word.Characters)
-            outWordArray.push(word)
-        }
-    })
+    }
 })
 
-console.log(outWordArray)
+// console.log(outWordArray)
+console.log("[extracted]")
+const outWord = []
+outWordArray.forEach((part, i) => {
+    console.log(`Part ${i} : ${part.length}`)
+    outWord.push({ Word : part })
+})
 
 // xml組み立て
 const outObj = { Words: {}}
@@ -49,7 +67,7 @@ outObj["Words"]["Author"] = ""
 outObj["Words"]["Language"] = xmlObj.Words.Language
 outObj["Words"]["Signature"] = Math.floor(Math.random()*10000)
 outObj["Words"]["Memo"] = "Extracted by wt-word-extractor"
-outObj["Words"]["Part"] = [ { Word: outWordArray} ]
+outObj["Words"]["Part"] = outWord
 
 const outXml = builder.build(outObj)
 
